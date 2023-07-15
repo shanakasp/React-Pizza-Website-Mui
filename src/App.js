0e033bd0dd7129804a60c543948ef9dcee77b61e
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-15 12:57:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-
-
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
       <Router>
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
